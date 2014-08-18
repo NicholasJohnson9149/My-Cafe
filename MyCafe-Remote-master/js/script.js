849ca29d69325ae1b1df95b6bc8b3dcb59385d3c
wrobbinz <Broccoli@Broccoli-Cheeses-MacBook-Pro.local>
2014-08-18 15:53:02 -0700

    Get/Post edits

--- MyCafe-Remote-master/js/script.js
+++ MyCafe-Remote-master/js/script.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
-	var deviceID    = "DeviceAccessTokenNumberGoeshere!";
-	 var accessToken = "YourAcountsTokenGoesHereCanBefoundinSettingofSparkBuildIDE";
-	var brew = 0; // 1 = brewing, 0 = idle
+	var deviceID    = "55ff6c065075555324421487";
+	var accessToken = "6249ef747c68f32bde0e4deb2f1395705e59a84b";
+	var brew = -1; // 1 = brewing, -1 = idle
 	var command = ""; // sendData type
 	var grindTime = 0; // how long to grind
 	var grindSent = 0; // check if grind ended (event listener check)
 	var brewSent = 0;
 	// initialize by checking if MyCafe is ON or actively brewing
-	requestURL = "https://api.spark.io/v1/devices/" +deviceID + "/" + brew + "/";
+	requestURL = "https://api.spark.io/v1/devices/" +deviceID + "/" + "brew" + "/";
     
     //Brew Function 
-	brew = $.get( requestURL, { access_token: accessToken });
-	// if so, adjust interface accordingly
-	if (brew == 1){
-		$('.brew').empty().html('STOP BREWING');
-	}
+	//brew = $.get( requestURL, { access_token: accessToken });
+	//alert(brew);
+	// if brewing, adjust interface accordingly
+	// if (brew == 1){
+	// 	$('.brew').empty().html('STOP BREWING');
+	// }
     // grind button logic
 	$('.grind').on('click', function(){
 		$(this).fadeOut(250, function(){
@@ -25,41 +26,42 @@ $(document).ready(function() {
 	$('.fine-grind').on('click', function(){
 		grindTime = 30000;
 		command = "grind";
-		grindSent = sendData(command, grindTime);
+		grindSent = sendData(command, grindTime); // send command = 'grind' and duration to grind: 30s
 		grindListen(grindSent); // set up event listener, if grindSent = 1, post was successfull
 	});
 	$('.medium-grind').on('click', function(){
 		grindTime = 20000;
 		command = "grind";
 		grindSent = sendData(command, grindTime);
-		grindListen(grindSent); // set up event listener, if grindSent = 1, post was successfull
+		grindListen(grindSent);
 
 	});
 	$('.coarse-grind').on('click', function(){
 		grindTime = 10000;
 		command = "grind";
 		grindSent = sendData(command, grindTime);
-		grindListen(grindSent); // set up event listener, if grindSent = 1, post was successfull
+		grindListen(grindSent);
 
 	});
 	// brew button logic
 	$('.brew').on('click', function(){
 		command = "brew";
     	if (brew === 1){
-    		brew = "brew";
-    		$('.grind').empty().html('BREW');
+    		$('.brew').empty().html('BREW');
+    		brew = -1;
     	}
-    	if (brew === 0){
-    		brew = "offBrew";
+    	else if (brew === -1){
     		$('.brew').empty().html('STOP BREWING');
+    		brew = 1;
     	}
-		brewSent = sendData(command, brew);
+		brewSent = sendData(command, brew); // send command: 'brew' and whether to start/stop brewing
+		
 
 	});
 
 
 	var sendData = function(command, data){
-		requestURL = "https://api.spark.io/v1/devices/" +deviceID + "/" + command + "/";
+		requestURL = "https://api.spark.io/v1/devices/" +deviceID + "/" + command;
         
 		if (command == "grind"){
 			$.post( requestURL, { params: data, access_token: accessToken }, function(){return 1;});
@@ -69,10 +71,9 @@ $(document).ready(function() {
 		}
 	};
 	var grindListen = function(grindSent){
-		//grindSent = 1;
+		grindSent = 1;
 		if (grindSent == 1){
-			alert('My Coffee Rrocks');
-			var evtSource = new EventSource(requestURL);
+			var evtSource = new EventSource("https://api.spark.io/v1/devices/" + deviceID + "/events/" + command + "?access_token=" + accessToken);
 			evtSource.addEventListener("ping", function(e){
 				var obj = JSON.parse(e.data);
 
@@ -81,7 +82,7 @@ $(document).ready(function() {
 					$('.grind').fadeIn(250);
 					loading();
 				});
-				if (obj === "Go get Coffee"){
+				if (obj === "grindDone"){
 					clearTimeout(noResponse);
 					evtSouce.close();
 					$('.grind').empty().html('GRIND COMPLETE').delay(3000).empty().html('GRIND');
